eb0e1d00c45fa56749fbfb3c24f2b3a7aa2062e9
lunarifish <messenger@lunariio.work>
2024-07-31 09:40:50 +0800

    asdsadasda

---
+++ omz.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sh -c "$(curl -fsSL https://gitee.com/Devkings/oh_my_zsh_install/raw/master/install.sh)"
---
+++ vscode-extensions.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+code --install-extension njpwerner.autodocstring
+code --install-extension alefragnani.bookmarks
+code --install-extension ms-vscode.cpptools-extension-pack
+code --install-extension xaver.clang-format
+code --install-extension llvm-vs-code-extensions.vscode-clangd
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension usernamehw.errorlens
+code --install-extension tamasfe.even-better-toml
+code --install-extension github.vscode-github-actions
+code --install-extension github.copilot
+code --install-extension github.copilot-chat
+code --install-extension eamodio.gitlens
+code --install-extension qvist.jetbrains-new-ui-dark-theme
+code --install-extension ritwickdey.liveserver
+code --install-extension donjayamanne.python-extension-pack
+code --install-extension kevinrose.vsc-python-indent
+code --install-extension spencerwmiles.vscode-task-buttons
+code --install-extension vscode-icons-team.vscode-icons
+code --install-extension wakatime.vscode-wakatime
+code --install-extension esbenp.prettier-vscode
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension yzane.markdown-pdf
+code --install-extension davidanson.vscode-markdownlint
