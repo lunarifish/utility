aaa055106da0f5ca27b403c66e68fcf01ed10179
Haonan Liu <messenger@lunariio.work>
2025-03-16 14:43:42 +0800

    Update vscode-extensions-minimal.sh

--- vscode-extensions-minimal.sh
+++ vscode-extensions-minimal.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-code --install-extension dan-c-underwood.arm
-code --install-extension usernamehw.errorlens
-code --install-extension surajbarkale.ninja
-code --install-extension peterj.proto
-code --install-extension llvm-vs-code-extensions.vscode-clangd
-code --install-extension ms-vscode.cpptools-extension-pack
-code --install-extension cschlosser.doxdocgen
-code --install-extension tamasfe.even-better-toml
-code --install-extension github.vscode-github-actions
-code --install-extension github.copilot
-code --install-extension github.copilot-chat
-code --install-extension oderwat.indent-rainbow
-code --install-extension ms-vscode.makefile-tools
-code --install-extension esbenp.prettier-vscode
-code --install-extension donjayamanne.python-extension-pack
-code --install-extension spencerwmiles.vscode-task-buttons
-code --install-extension gruntfuggly.todo-tree
-code --install-extension wakatime.vscode-wakatime
-code --install-extension ibm.output-colorizer
+code --install-extension dan-c-underwood.arm &
+code --install-extension usernamehw.errorlens &
+code --install-extension surajbarkale.ninja &
+code --install-extension peterj.proto &
+code --install-extension llvm-vs-code-extensions.vscode-clangd &
+code --install-extension ms-vscode.cpptools-extension-pack &
+code --install-extension cschlosser.doxdocgen &
+code --install-extension tamasfe.even-better-toml &
+code --install-extension github.vscode-github-actions &
+code --install-extension github.copilot &
+code --install-extension github.copilot-chat &
+code --install-extension oderwat.indent-rainbow &
+code --install-extension ms-vscode.makefile-tools &
+code --install-extension esbenp.prettier-vscode &
+code --install-extension donjayamanne.python-extension-pack &
+code --install-extension spencerwmiles.vscode-task-buttons &
+code --install-extension gruntfuggly.todo-tree &
+code --install-extension wakatime.vscode-wakatime &
+code --install-extension ibm.output-colorizer &
 code --install-extension ms-iot.vscode-ros
