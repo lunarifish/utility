f885fc74820e182964a911532f54b907a60e4094
Haonan Liu <messenger@lunariio.work>
2025-03-16 14:43:58 +0800

    Update vscode-extensions.sh

--- vscode-extensions.sh
+++ vscode-extensions.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
-code --install-extension dan-c-underwood.arm
-code --install-extension alefragnani.bookmarks
-code --install-extension usernamehw.errorlens
-code --install-extension surajbarkale.ninja
-code --install-extension peterj.proto
-code --install-extension llvm-vs-code-extensions.vscode-clangd
-code --install-extension ms-vscode.cpptools-extension-pack
-code --install-extension cschlosser.doxdocgen
-code --install-extension tamasfe.even-better-toml
-code --install-extension marus25.cortex-debug
-code --install-extension vscode-icons-team.vscode-icons
-code --install-extension qvist.jetbrains-new-ui-dark-theme
-code --install-extension adpyke.codesnap
-code --install-extension github.vscode-github-actions
-code --install-extension github.copilot
-code --install-extension github.copilot-chat
-code --install-extension oderwat.indent-rainbow
-code --install-extension ritwickdey.liveserver
-code --install-extension ms-vscode.makefile-tools
-code --install-extension esbenp.prettier-vscode
-code --install-extension donjayamanne.python-extension-pack
-code --install-extension fill-labs.dependi
-code --install-extension rust-lang.rust-analyzer
-code --install-extension spencerwmiles.vscode-task-buttons
-code --install-extension gruntfuggly.todo-tree
-code --install-extension tonybaloney.vscode-pets
-code --install-extension wakatime.vscode-wakatime
-code --install-extension curlybrackets.markdown-word-count
-code --install-extension mathworks.language-matlab
-code --install-extension ibm.output-colorizer
-code --install-extension ms-iot.vscode-ros
+code --install-extension dan-c-underwood.arm &
+code --install-extension alefragnani.bookmarks &
+code --install-extension usernamehw.errorlens &
+code --install-extension surajbarkale.ninja &
+code --install-extension peterj.proto &
+code --install-extension llvm-vs-code-extensions.vscode-clangd &
+code --install-extension ms-vscode.cpptools-extension-pack &
+code --install-extension cschlosser.doxdocgen &
+code --install-extension tamasfe.even-better-toml &
+code --install-extension marus25.cortex-debug &
+code --install-extension vscode-icons-team.vscode-icons &
+code --install-extension qvist.jetbrains-new-ui-dark-theme &
+code --install-extension adpyke.codesnap &
+code --install-extension github.vscode-github-actions &
+code --install-extension github.copilot &
+code --install-extension github.copilot-chat &
+code --install-extension oderwat.indent-rainbow &
+code --install-extension ritwickdey.liveserver &
+code --install-extension ms-vscode.makefile-tools &
+code --install-extension esbenp.prettier-vscode &
+code --install-extension donjayamanne.python-extension-pack &
+code --install-extension fill-labs.dependi &
+code --install-extension rust-lang.rust-analyzer &
+code --install-extension spencerwmiles.vscode-task-buttons &
+code --install-extension gruntfuggly.todo-tree &
+code --install-extension tonybaloney.vscode-pets &
+code --install-extension wakatime.vscode-wakatime &
+code --install-extension curlybrackets.markdown-word-count &
+code --install-extension mathworks.language-matlab &
+code --install-extension ibm.output-colorizer &
+code --install-extension ms-iot.vscode-ros &
 code --install-extension ms-azuretools.vscode-docker
