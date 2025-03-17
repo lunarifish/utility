190dcfcaaf6b42ce80a7f422bfe28c0f862635ec
Haonan Liu <messenger@lunariio.work>
2025-03-17 18:59:37 +0800

    Update vscode-extensions-minimal.sh

--- vscode-extensions-minimal.sh
+++ vscode-extensions-minimal.sh
@@ -18,4 +18,5 @@ code --install-extension spencerwmiles.vscode-task-buttons &
 code --install-extension gruntfuggly.todo-tree &
 code --install-extension wakatime.vscode-wakatime &
 code --install-extension ibm.output-colorizer &
-code --install-extension ms-iot.vscode-ros
+code --install-extension ms-iot.vscode-ros &
+code --install-extension kylinideteam.cmake-intellisence
